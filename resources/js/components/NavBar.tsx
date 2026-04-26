8fa80a2825fcc9a71907b093634ba893d8501485
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-04-26 14:23:28 -0600

    update navbar a lot

--- resources/js/components/NavBar.tsx
+++ resources/js/components/NavBar.tsx
@@ -1,16 +1,23 @@
 import NavItem from "./NavItem";
-
+import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision";
+import { FaGithub } from 'react-icons/fa';
 const Navbar = () => {
     return (
         <div>
-            <nav className="flex justify-end border h-48">
-                <ul className="flex">
+            <nav className="flex h-48 bg-zinc-200 dark:bg-zinc-800 text-zinc-800 dark:text-neutral-300">
+                <BackgroundBeamsWithCollision>
+                <div className="flex h-full w-full items-center">
+                    <img className="h-full [image-rendering:pixelated] aspect-square" src="images/spaceman.png" alt="Spaceman Logo" />
+                    <div className="font-extrabold text-5xl px-10 text-zinc-800 dark:text-amber-300"><p>ISS Urine Tank Display</p></div>
+                </div>
+                <ul className="flex h-32 w-full justify-end items-center">
                     <NavItem href="" label="Home" />      
                     <NavItem href="" label="Mods" />
                     <NavItem href="" label="FAQ" />
                     <NavItem href="" label="Contact" />
-                    <NavItem href="https://github.com/Chilllyy" label="Github" />
+                    <li className="px-5 mx-4"><a href="https://github.com/Chilllyy"><FaGithub className="h-12 w-12" /></a></li>
                 </ul>
+                </BackgroundBeamsWithCollision>
             </nav>
         </div>
     );
--- resources/js/components/NavItem.tsx
+++ resources/js/components/NavItem.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@inertiajs/react";
 
-export default function NavItem({ href, label }) {
-    return <li><Link href={href}>{label}</Link></li>;
+export default function NavItem({ href, label }: { href: string; label: string }) {
+    return <li className="px-5 mx-4 border rounded-2xl"><Link href={href}>{label}</Link></li>;
 }
