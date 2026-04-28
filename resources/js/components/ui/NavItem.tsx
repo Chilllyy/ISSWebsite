307b31905adaffbd30ae83c378bf452b6595076b
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-04-28 13:51:36 -0600

    add backdrop to nav item

--- resources/js/components/ui/NavItem.tsx
+++ resources/js/components/ui/NavItem.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@inertiajs/react";
 
 export default function NavItem({ href, label }: { href: string; label: string }) {
-    return <li className="px-5 mx-4 border rounded-2xl hover:scale-110 transition-all duration-300 ease-in-out transform"><Link href={href}>{label}</Link></li>;
+    return <li className={`${route().current() == label.toLowerCase() ? 'bg-zinc-400 dark:bg-zinc-600' : ''} px-5 mx-4 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out transform`}><Link href={href}>{label}</Link></li>;
 }
