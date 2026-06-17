6c94d7a6603976ff88f1919588cb4a1e4302a7b2
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-17 14:52:09 -0600

    update uesr agent check to not search for a specific version of okhttp

--- app/Http/Middleware/CheckUserAgent.php
+++ app/Http/Middleware/CheckUserAgent.php
@@ -18,7 +18,7 @@ class CheckUserAgent
     public function handle(Request $request, Closure $next): Response
     {
         $userAgent = $request->header('User-Agent');
-        if ($userAgent && str_contains($userAgent, 'okhttp/4.12.0')) {
+        if ($userAgent && str_contains($userAgent, 'okhttp/')) {
             Log::info("Received okhttp version 4.12.0 request, redirecting to new API");
             return Inertia::location(route('api.value'));
         }
